e4b3bbd1b837084c2812f7c20961f824f121993f
merlish <merlyn@starasterisk.com>
2012-06-04 01:52:45 +0100

    fix horrible eid bug (all eids were 0)... was appending to a most definitely non-empty slice :s

--- src/endlib/server/eid.go
+++ src/endlib/server/eid.go
@@ -1,5 +1,7 @@
 package server
 
+import "fmt"
+
 // TODO: make not terrible!
 
 func eidServer(eids chan int32) {
@@ -10,7 +12,8 @@ func eidServer(eids chan int32) {
     available := make([]int32, numEids)
 
     for i := 1; i < numEids + 1; i++ {
-        available = append(available, int32(i))
+        //available = append(available, int32(i))
+        available[i-1] = int32(i)
     }
 
     for {
@@ -21,11 +24,12 @@ func eidServer(eids chan int32) {
 
         select {
         case freed := <-eids: // someone returning an eid
-            // TODO: in debug mode only, check eid is not already in free list?
+            // TODO: in debug mode only: check eid is not already in free list?
 
             available = append(available, freed)
 
         case eids <- available[0]:  // someone trying to read an eid
+            fmt.Printf("dbg: eid server: just handed out %v... next eid is %v\n", int(available[0]), available[1])
             available = available[1:]
         }
     }
