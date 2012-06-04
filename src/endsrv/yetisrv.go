ab92924e4bf09d24bc90bac2e6d9a77fa77e3d5f
merlish <merlyn@starasterisk.com>
2012-06-04 01:19:14 +0100

    little change to indicate this is, in fact, yetisrv..

--- src/endsrv/yetisrv.go
+++ src/endsrv/yetisrv.go
@@ -1,10 +1,15 @@
 package main
 
+import "fmt"
+
 import "endlib"
 import "endlib/server"
 
 func main() {
     srv := server.New()
+
+    fmt.Printf("\nyetisrv: the endsrv fork for space-yeti!\n")
+
 	if !dispatch.Listen(srv) {
 		panic("failed to start server: dispatch.listen call failed.")
 	}
